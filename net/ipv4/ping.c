4ea28049ffa45637fc66ecbdf72b96bbd2eb0620
djStolen <djordjestojicevic@gmail.com>
2023-12-21 00:43:51 +0100

    Linux Kernel 3.10.40 compiles and runs (.rej cleaned up)
    Applied:
    /LineageOS/android/kernel/sony/msm8994$ patch -p1 -R <
    /home/dj/Downloads/linuxkernelpatches/patch-3.10.40-41
    
    Bugfix in revert:
    include/linux/list.h not reverted since list_next_entry() is needed:
    --------------------------------
    ../../../../../../kernel/sony/msm8994/drivers/crypto/msm/qcrypto.c: In
    function '_qcrypto_static_assign_engine':
    ../../../../../../kernel/sony/msm8994/drivers/crypto/msm/qcrypto.c:1970:3:
    error: implicit declaration of function 'list_next_entry'
    [-Werror=implicit-function-declaration]
       cp->next_engine = list_next_entry(pengine, elist);
       ^
    ../../../../../../kernel/sony/msm8994/drivers/crypto/msm/qcrypto.c:1970:46:
    error: 'elist' undeclared (first use in this function)
       cp->next_engine = list_next_entry(pengine, elist);
    
    Change-Id: I402fc9accee1d28a8ce7f36e0c7bfc11ef19951d

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -251,10 +251,9 @@ int ping_init_sock(struct sock *sk)
 {
 	struct net *net = sock_net(sk);
 	kgid_t group = current_egid();
-	struct group_info *group_info;
-	int i, j, count;
+	struct group_info *group_info = get_current_groups();
+	int i, j, count = group_info->ngroups;
 	kgid_t low, high;
-	int ret = 0;
 
 	if (sk->sk_family == AF_INET6)
 		inet6_sk(sk)->ipv6only = 1;
@@ -263,24 +262,17 @@ int ping_init_sock(struct sock *sk)
 	if (gid_lte(low, group) && gid_lte(group, high))
 		return 0;
 
-	group_info = get_current_groups();
-	count = group_info->ngroups;
 	for (i = 0; i < group_info->nblocks; i++) {
 		int cp_count = min_t(int, NGROUPS_PER_BLOCK, count);
 		for (j = 0; j < cp_count; j++) {
 			kgid_t gid = group_info->blocks[i][j];
 			if (gid_lte(low, gid) && gid_lte(gid, high))
-				goto out_release_group;
+				return 0;
 		}
 
 		count -= cp_count;
 	}
-
-	ret = -EACCES;
-
-out_release_group:
-	put_group_info(group_info);
-	return ret;
+	return -EACCES;
 }
 EXPORT_SYMBOL_GPL(ping_init_sock);
 
